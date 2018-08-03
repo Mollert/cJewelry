fc92487c359707f9a3f31e3e6d2a32aba1d7b738
Mollert <blueskies@carolina.rr.com>
2018-08-03 15:37:55 -0400

    First commit

---
+++ dBaseCJ.js
@@ -0,0 +1,31 @@
+
+const comments = [
+		{
+			"id": 0,
+			"replyId": "",
+			"date": "1:01:2018",
+			"name": "Patty",
+			"comment": "What a selection.  And all handmade!"
+		},
+		{
+			"id": 1,
+			"replyId": "",
+			"date": "2:10:2018",
+			"name": "Henry",
+			"comment": "Just found you . . . I'll be back"
+		},
+		{
+			"id": 2,
+			"replyId": "",
+			"date": "4:18:2018",
+			"name": "Roberta",
+			"comment": "I like to talk to you about making me something special"
+		},
+		{
+			"id": 3,
+			"replyId": "1",
+			"date": "5:1:2018",
+			"name": "April",
+			"comment": "I enjoyed what I saw too."
+		}
+	];
